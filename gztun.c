9fa39e36d43e239bb9a634b600d12acfda000862
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2026-02-07 21:54:33 +0900

    refactor: restructure zstd context management and enhance MTU handling

--- gztun.c
+++ gztun.c
@@ -15,21 +15,36 @@
 
 #define DRV_NAME "gztun"
 #define ZSTD_COMPRESSION_LEVEL 1
-#define BUFSIZE 256
 #define TUN_PORT 9999
 
-struct zstd_ctx {
-  void *workspace;
-  size_t workspace_size;
-  zstd_parameters params;
+struct percpu_ctx {
+  void *zstd_wksp;
+  size_t zstd_wksp_size;
+  void *dst_buf;
+  size_t dst_buf_size;
 };
 
-static struct zstd_ctx __percpu *zstd_ctx_pool;
+static struct percpu_ctx __percpu *percpu_ctxs;
 
 struct gztun_struct {
   struct net_device *redirect_dev;
   netdevice_tracker tracker;
+  zstd_parameters params;
 };
+
+static size_t zstd_max_input(size_t limit) {
+  size_t low = 0, high = limit, mid;
+
+  while (low < high) {
+    mid = low + (high - low + 1) / 2;
+    if (zstd_compress_bound(mid) <= limit)
+      low = mid;
+    else
+      high = mid - 1;
+  }
+
+  return low;
+}
 
 static void free_gztun_struct(struct gztun_struct *gztun) {
   if (gztun->redirect_dev) {
@@ -96,15 +111,15 @@ static int stretch_skb_size(struct sk_buff *skb, int size_diff) {
 
 static netdev_tx_t gztun_start_xmit(struct sk_buff *skb, struct net_device *dev) {
   struct gztun_struct *gztun;
-  struct zstd_ctx *ctx;
+  struct percpu_ctx *ctx;
   zstd_cctx *cctx;
   int ret;
 
-  u8 dst_buf[BUFSIZE];
   int len_diff, udph_off, payload_off;
   size_t old_size, new_size;
 
   pr_info("gztun: start_xmit called, len=%u\n", skb->len);
+  gztun = netdev_priv(dev);
 
   if (check_proto(skb) != 0) {
     pr_err("gztun: packet is not UDP to port %d, dropping\n", TUN_PORT);
@@ -120,24 +135,25 @@ static netdev_tx_t gztun_start_xmit(struct sk_buff *skb, struct net_device *dev)
     goto drop;
   }
 
-  ctx = get_cpu_ptr(zstd_ctx_pool);
-  cctx = zstd_init_cctx(ctx->workspace, ctx->workspace_size);
+  ctx = get_cpu_ptr(percpu_ctxs);
+  cctx = zstd_init_cctx(ctx->zstd_wksp, ctx->zstd_wksp_size);
   if (!cctx) {
     pr_err("gztun: failed to initialize zstd cctx\n");
-    put_cpu_ptr(zstd_ctx_pool);
+    put_cpu_ptr(percpu_ctxs);
     goto drop;
   }
 
-  new_size = zstd_compress_cctx(cctx, dst_buf, BUFSIZE, skb->data + payload_off, old_size, &ctx->params);
+  new_size = zstd_compress_cctx(cctx, ctx->dst_buf, ctx->dst_buf_size,
+                                skb->data + payload_off, old_size, &gztun->params);
   if (zstd_is_error(new_size)) {
     pr_err("gztun: zstd compression failed: %s\n", zstd_get_error_name(new_size));
     zstd_free_cctx(cctx);
-    put_cpu_ptr(zstd_ctx_pool);
+    put_cpu_ptr(percpu_ctxs);
     goto drop;
   }
 
   zstd_free_cctx(cctx);
-  put_cpu_ptr(zstd_ctx_pool);
+  put_cpu_ptr(percpu_ctxs);
 
   len_diff = new_size - old_size;
   ret = stretch_skb_size(skb, len_diff);
@@ -146,7 +162,7 @@ static netdev_tx_t gztun_start_xmit(struct sk_buff *skb, struct net_device *dev)
     goto drop;
   }
 
-  memcpy(skb->data + payload_off, dst_buf, new_size);
+  memcpy(skb->data + payload_off, ctx->dst_buf, new_size);
   struct iphdr *iph = (struct iphdr *)skb->data;
   struct udphdr *udph = (struct udphdr *)(skb->data + udph_off);
   udph->len = htons(sizeof(struct udphdr) + new_size);
@@ -157,7 +173,6 @@ static netdev_tx_t gztun_start_xmit(struct sk_buff *skb, struct net_device *dev)
   iph->check = 0;
   ip_send_check(iph);
 
-  gztun = netdev_priv(dev);
   skb->dev = gztun->redirect_dev;
   dev_queue_xmit(skb);
 
@@ -193,6 +208,40 @@ static int gztun_validate(struct nlattr *tb[], struct nlattr *data[], struct net
   return 0;
 }
 
+static int alloc_percpu_ctxs(zstd_parameters *params, int parent_mtu) {
+  int cpu;
+  size_t zstd_wksp_size;
+  size_t dst_buf_size;
+  struct percpu_ctx *ctx;
+
+  zstd_wksp_size = zstd_cctx_workspace_bound(&params->cParams);
+  dst_buf_size = parent_mtu;
+
+  for_each_possible_cpu(cpu) {
+    ctx = per_cpu_ptr(percpu_ctxs, cpu);
+    if (ctx->zstd_wksp_size < zstd_wksp_size) {
+      kvfree(ctx->zstd_wksp);
+      ctx->zstd_wksp = kvmalloc(zstd_wksp_size, GFP_KERNEL);
+      if (!ctx->zstd_wksp) {
+        pr_err("gztun: failed to allocate zstd workspace for CPU %d\n", cpu);
+        return -ENOMEM;
+      }
+      ctx->zstd_wksp_size = zstd_wksp_size;
+    }
+    if (ctx->dst_buf_size < dst_buf_size) {
+      kvfree(ctx->dst_buf);
+      ctx->dst_buf = kvmalloc(dst_buf_size, GFP_KERNEL);
+      if (!ctx->dst_buf) {
+        pr_err("gztun: failed to allocate dst buffer for CPU %d\n", cpu);
+        return -ENOMEM;
+      }
+      ctx->dst_buf_size = dst_buf_size;
+    }
+  }
+
+  return 0;
+}
+
 static int gztun_newlink(struct net_device *dev, struct rtnl_newlink_params *params, struct netlink_ext_ack *extack) {
   pr_info("gztun: newlink called for device %s\n", dev->name);
 
@@ -204,6 +253,7 @@ static int gztun_newlink(struct net_device *dev, struct rtnl_newlink_params *par
 
   gztun = netdev_priv(dev);
   gztun->redirect_dev = NULL;
+  gztun->params = zstd_get_params(ZSTD_COMPRESSION_LEVEL, 0);
   tb = params->tb;
 
   if (tb[IFLA_LINK]) {
@@ -221,12 +271,24 @@ static int gztun_newlink(struct net_device *dev, struct rtnl_newlink_params *par
     return -EINVAL;
   }
 
+  int parent_mtu = READ_ONCE(gztun->redirect_dev->mtu);
+  int mtu = zstd_max_input(parent_mtu);
+  WRITE_ONCE(dev->mtu, mtu);
+  pr_info("gztun: set MTU to %d (parent MTU %d)\n", mtu, parent_mtu);
+  err = alloc_percpu_ctxs(&gztun->params, parent_mtu);
+  if (err != 0) {
+    free_gztun_struct(gztun);
+    return err;
+  }
+
   err = register_netdevice(dev);
   if (err) {
     pr_err("gztun: register_netdev failed: %d\n", err);
     free_gztun_struct(gztun);
+    return err;
   }
-  return err;
+
+  return 0;
 }
 
 static void gztun_dellink(struct net_device *dev, struct list_head *head) {
@@ -244,48 +306,35 @@ static struct rtnl_link_ops gztun_link_ops = {
     .priv_size = sizeof(struct gztun_struct),
 };
 
-static void free_zstd_ctx_pool(void) {
+static void free_percpu_ctxs(void) {
   int cpu;
+
+  if (!percpu_ctxs)
+    return;
+
   for_each_possible_cpu(cpu) {
-    struct zstd_ctx *ctx = per_cpu_ptr(zstd_ctx_pool, cpu);
-    if (ctx->workspace) {
-      kvfree(ctx->workspace);
-      ctx->workspace = NULL;
-    }
+    struct percpu_ctx *ctx = per_cpu_ptr(percpu_ctxs, cpu);
+    if (ctx->zstd_wksp)
+      kvfree(ctx->zstd_wksp);
+    if (ctx->dst_buf)
+      kvfree(ctx->dst_buf);
   }
-  free_percpu(zstd_ctx_pool);
-  zstd_ctx_pool = NULL;
+  free_percpu(percpu_ctxs);
+  percpu_ctxs = NULL;
 }
 
 static int __init gztun_netdev_init(void) {
-  int err, cpu;
-  zstd_parameters params;
-  size_t workspace_size;
+  int err;
   pr_info("gztun: initializing\n");
 
-  params = zstd_get_params(ZSTD_COMPRESSION_LEVEL, 0);
-  workspace_size = zstd_cstream_workspace_bound(&params.cParams);
-
-  zstd_ctx_pool = alloc_percpu(struct zstd_ctx);
-  if (!zstd_ctx_pool)
+  percpu_ctxs = alloc_percpu(struct percpu_ctx);
+  if (!percpu_ctxs)
     return -ENOMEM;
-
-  for_each_possible_cpu(cpu) {
-    struct zstd_ctx *ctx = per_cpu_ptr(zstd_ctx_pool, cpu);
-    ctx->params = params;
-    ctx->workspace_size = workspace_size;
-    ctx->workspace = kvmalloc(workspace_size, GFP_KERNEL);
-    if (!ctx->workspace) {
-      pr_err("gztun: failed to allocate zstd workspace for CPU %d\n", cpu);
-      free_zstd_ctx_pool();
-      return -ENOMEM;
-    }
-  }
 
   err = rtnl_link_register(&gztun_link_ops);
   if (err < 0) {
     pr_err("gztun: failed to register rtnl link ops\n");
-    free_zstd_ctx_pool();
+    free_percpu_ctxs();
     return err;
   }
 
@@ -295,7 +344,7 @@ static int __init gztun_netdev_init(void) {
 static void __exit gztun_netdev_exit(void) {
   pr_info("gztun: exiting\n");
   rtnl_link_unregister(&gztun_link_ops);
-  free_zstd_ctx_pool();
+  free_percpu_ctxs();
 }
 
 module_init(gztun_netdev_init);
